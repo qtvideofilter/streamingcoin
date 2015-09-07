7e742bd6cfc778fd2e7bc82ddda312971a1ff2d8
test@test.com <test@test.com>
2015-09-07 21:40:05 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Phreak (PHR)
+
+Full-PoS
+Max supply: 30 Millions
+Ito: 100 satoshi 
+PoS: 10% yearly
+Maturity: 30 Confirmations
+Min. Stake Age: 6 Hours
---
+++ src/genesis.h
@@ -0,0 +1,13 @@
+#ifndef GENESIS_H
+#define GENESIS_H
+
+#include "bignum.h"
+
+// Genesis block 0 hash
+
+static const uint256 hashGenesisBlock("0x00000b2e7193898c260b5971eace86f7bfd73ed92968027e495bc9964517cbab");
+static const uint256 hashGenesisBlockTestNet("");
+
+
+
+#endif // GENESIS_H
